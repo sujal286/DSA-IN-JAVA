fa1f59fce88f900c80c0d2f4f06c9ee1a0bb7c07
Sujal Saha <sujalsaha14@gmail.com>
2024-07-02 13:47:35 +0530

    Insertion Sort done

---
+++ InsertionSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class InsertionSort 
+{
+    public static void insertionSort(int arr[],int s)
+    {
+        for(int i=1;i<s;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key)
+            {
+                arr[j+1]=arr[j];
+                j-=1;
+            }
+            arr[j+1]=key;
+        }
+    }
+    public static void display(int arr[],int s)
+    {
+        for(int i=0;i<s;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+  public static void main(String args[])
+  {
+    System.out.println("Enter size of array:");
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int arr[]=new int[size];
+    System.out.println("Enter elements of array:");
+    for(int i=0;i<arr.length;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
+    insertionSort(arr,size);
+    display(arr,size);
+  }  
+}
