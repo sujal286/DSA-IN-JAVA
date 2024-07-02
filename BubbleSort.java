7381acb4216acdce89548e506c9d378b26e86d31
Sujal Saha <sujalsaha14@gmail.com>
2024-07-02 13:33:27 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class BubbleSort 
+{
+   public static void bubbleSort(int arr[],int s)
+    {
+        for(int i=0;i<s-1;i++)
+        {
+            for(int j=0;j<s-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void display(int arr[],int s)
+    {
+        for(int i=0;i<s;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        System.out.println("Enter size of array:");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter elements of array:");
+        for(int i=0;i<arr.length;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+        bubbleSort(arr,size); 
+        display(arr,size);
+    }
+}
