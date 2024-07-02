11e0f87b4f52d7fb5967dc951d84606e3ecf414d
Sujal Saha <sujalsaha14@gmail.com>
2024-07-02 13:22:15 +0530

    Linear Search

---
+++ LinearSearch.java
@@ -0,0 +1,35 @@
+//LINEAR SEARCH
+import java.util.*;
+class LinearSearch
+{
+    public static int linearSearch(int arr[],int k,int s)
+    {
+       for(int i=0;i<s;i++)
+       {
+        if(k==arr[i])
+        {
+            return i;
+        }
+       }
+       return -1;
+    }
+    public static void main(String args[])
+    {
+        System.out.println("Enter size of array:");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter elements of array:");
+        for(int i=0;i<arr.length;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element you want to search:");
+        int key=sc.nextInt();
+        int p=linearSearch(arr,key,size);
+        if(p==-1)
+        System.out.println("Cannot find the element");
+        else
+        System.out.println(key+" found at:"+p);
+    }
+}
