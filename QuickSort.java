8796cae2810c21c29183523ed1ee19e3768979f9
Sujal Saha <sujalsaha14@gmail.com>
2024-07-04 20:37:11 +0530

    QuickSort done

---
+++ QuickSort.java
@@ -0,0 +1,60 @@
+//TC-O(nlogn)
+//SC-O(n)
+import java.util.Scanner;
+class QuickSort
+{
+
+    public static void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    public static int partition(int arr[],int low,int high)
+    {
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<=pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,high);
+        return(i+1);
+    }
+
+    public static void display(int arr[],int size)
+    {
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void quickSort(int arr[],int low,int high)
+    {
+        if(low<high)
+        {
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        quickSort(arr, 0, arr.length - 1);
+        display(arr, size);
+    }
+}
