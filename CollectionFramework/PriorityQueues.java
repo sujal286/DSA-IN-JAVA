4913875595d93477fc9dccb5f0f44e330ba367d0
Sujal Saha <sujalsaha14@gmail.com>
2024-09-25 21:17:51 +0530

    pq done

---
+++ CollectionFramework/PriorityQueues.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueues {
+    
+    public static void main(String args[])
+    {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        pq.offer(70);
+        pq.offer(20);
+        pq.offer(30);
+        pq.offer(10);
+
+        System.out.println(pq); 
+
+        System.out.println(pq.size());
+
+        if(pq.contains(20))
+        {
+            System.out.println("Yes,it contains 20");
+        }
+
+        pq.poll();
+        System.out.println(pq);
+
+        System.out.println(pq.peek());
+    }
+}
