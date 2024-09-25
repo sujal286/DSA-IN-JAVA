181fed669f800edb354f6bcf85e232eb330c1607
Sujal Saha <sujalsaha14@gmail.com>
2024-09-25 21:19:08 +0530

    pq done

--- CollectionFramework/PriorityQueues.java
+++ CollectionFramework/PriorityQueues.java
@@ -1,10 +1,11 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueues {
     
     public static void main(String args[])
     {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
 
         pq.offer(70);
         pq.offer(20);
