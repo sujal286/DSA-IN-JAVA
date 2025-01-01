e5dfaca3de50c46db8cab19d4238da58f5751dd7
Sujal Saha <sujalsaha14@gmail.com>
2025-01-01 22:33:38 +0530

    Infix to Postfix

---
+++ Stack/Infixtopostfix.java
@@ -0,0 +1,103 @@
+import java.io.*;
+class Stacks{
+
+    char arr[]=new char[100];
+    int top=-1;
+
+    void push(char c)
+    {
+        try{
+             arr[++top]=c;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+    char pop()
+    {
+        return arr[top--];
+    }
+
+    char peek()
+    {
+        return arr[top];
+    }
+
+    boolean isEmpty()
+    {
+        if(top==-1)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Infixtopostfix{
+
+    static Stacks obj=new Stacks();
+    public static void main(String args[]) throws IOException{
+        
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Infix expression:");
+        String infix=br.readLine();
+
+        System.out.println("Conversion from infix to postfix:"+toPostfix(infix));
+    }
+
+    private static String toPostfix(String infix)
+    {
+      char symbol;
+      String postfix="";
+
+      for(int i=0;i<infix.length();i++)
+      {
+        symbol=infix.charAt(i);
+
+        if(Character.isLetter(symbol))
+        {
+          postfix+=symbol;
+        }
+        else if(symbol=='(')
+        {
+            obj.push(symbol);
+        }
+        else if(symbol==')')
+        {
+            while(obj.peek()!='(')
+            {
+                postfix+=obj.pop();
+            }
+            obj.pop();
+        }
+        else
+        {
+            while (!obj.isEmpty() && obj.peek() != '(' && prec(symbol) <= prec(obj.peek()))
+            {
+                postfix+=obj.pop();
+            }
+            obj.push(symbol);
+        }
+      }
+      while(!obj.isEmpty())
+      {
+        postfix+=obj.pop();
+      }
+      return postfix;
+    }
+
+    static int prec(char c)
+    {
+        if(c=='+' || c=='-')
+        return 1;
+        else if(c=='/' || c=='*')
+        return 2;
+
+        return 0;
+    }
+}
+
+
